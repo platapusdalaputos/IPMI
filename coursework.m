cdd34371ecf12e6a6c06293611afbd2519d167cf
ycgtucl <53170715+platapusdalaputos@users.noreply.github.com>
2021-05-16 13:10:20 +0100

    Adding final coursework

--- coursework.m
+++ coursework.m
@@ -59,93 +59,33 @@ contour(imgTest3','r');
 
 %% Performing the demons Registration
 
+% Testing out the parameters
 
 % for i = 2:4
-    sigma_elastic = 1.4;
-    sigma_fluid= 1.2;
-    num_lev = 1;
-    use_composition= false;
-    
+%     sigma_elastic = [1.0, 1.1, 1.2, 1.3, 1.4, 1.5];
+%     sigma_fluid= [1.0, 1.1, 1.2, 1.3, 1.4, 1.5];
+%     num_lev = [1, 2, 3, 4, 5, 6, 7];
+%     use_composition= false;
+%     
 %     lmsd = performRegimg(12,imgsAtlas, imgsTune,sigma_elastic, sigma_fluid, num_lev, use_composition);
-
+% 
 %     performRegimg(i,imgsAtlas, imgsTune,sigma_elastic(i), sigma_fluid(i), num_lev(i), use_composition);
 % end 
 
-[lmsd_map] = performRegimg(imgsAtlas, imgsTune, sigma_elastic, sigma_fluid, num_lev, use_composition);
+% Optimal Parameters
 
-% Pre Function code
-% % Z counter for the ith loop
-% 
-% lmsd_map = [];
-% storemask1 = zeros(3,5,340,270);
-% storemask2 = zeros(3,5,340,270);
-%     Z = 1;
-%     for i = 1:3
-%         
-%         % Importing the tuning images  
-%         imgTune1 = imrotate(double(cell2mat(imgsTune(Z))),-90);
-%         imgTune2 = flipud(double(cell2mat(imgsTune(Z+1))));
-%         imgTune3 = flipud(double(cell2mat(imgsTune(Z+2))));       
-%         
-%         % Displaying the tuning images
-%         figure;
-%         dispImage(imgTune1); hold on; imcontour(imgTune2,'g');imcontour(imgTune3,'r');
-%         title('tune');
-%         pause(1.0)
-%         
-%         % counters for the ith and jth loop respectively 
-%         Z = Z+3;
-%         X = 1;
-% 
-%         for j = 1:5
-%            
-%             % Importing the Atlas images
-%             imgAtlas1 = imrotate(double(cell2mat(imgsAtlas(X))),-90);
-%             imgAtlas2 = imrotate(double(cell2mat(imgsAtlas(X+1))),-90);
-%             imgAtlas3 = imrotate(double(cell2mat(imgsAtlas(X+2))),-90); 
-%            
-%             % Computing the registration          
-%             [warp_img_loop, def_field_loop]= demonsReg(imgAtlas1, imgTune1,sigma_elastic, sigma_fluid, num_lev, use_composition);
-%             
-%             % Re-sampling the segmented images with the calc def field
-%             resamp_img = resampImageWithDefField(imgAtlas2, def_field_loop, 'linear' , NaN);
-%             resamp_img2 = resampImageWithDefField(imgAtlas3, def_field_loop, 'linear' , NaN);
-%                         
-%             % ensuring the resampled brain-stem image is a binary image
-%             mask1 = resamp_img;
-%             mask1(resamp_img>0) = 1;
-%             mask1(isnan(resamp_img)) = 0; 
-%             storemask1(i,j,:,:)= mask1;
-%            
-%             % ensuring the resampled spinal cord image is a binary image
-%             mask2 = resamp_img2;
-%             mask2(resamp_img2>0) = 1;
-%             mask2(isnan(resamp_img2)) = 0;
-%             storemask2(i,j,:,:)= mask2;
-%             
-%             % displaying the registered and def field warped images  
-%             dispImage(warp_img_loop); 
-%             hold on; imcontour(mask1','g') ; imcontour(mask2','r');
-%             ax = gca;
-%             title('Atlas');
-%             pause(1.0);
-% 
-%             % Exporting the trial and error images
-%             % exportgraphics(ax,fullfile('./Coursework/ParamImages', sprintf('Param_%d_tune_%d_atlas_%d PV%d_%d_%d_%d.jpg',l,i,j,sigma_elastic(l),sigma_fluid(l),num_lev(l),use_composition)),'Resolution',300)
-%             
-%             % Calculating the LMSD 
-%             lmsd_map(i,j,:,:) = calcLMSD(warp_img_loop,imgTune1, 20);
-%             
-%             
-%             % X counter for the jth loop 
-%             X = X+3;
-%             
-%         end 
-%  
-% 
-%     end
-% %    
-%% Calculating MAS
+sigma_elastic = 1.4;
+sigma_fluid= 1.2;
+num_lev = 5;
+use_composition= false;
+
+sourceNumber = 3;
+
+% Calculating for the tuning
+[lmsd_map, storemask1, storemask2] = performRegimg(imgsAtlas, imgsTune, sigma_elastic, sigma_fluid, num_lev, use_composition);
+% Calculating for the testing
+
+%% Calculating and displaying MAS for tuning images
 
 % Tuning Images
 
@@ -153,17 +93,124 @@ imgTuned1 = imrotate(double(cell2mat(imgsTune(1))),-90);
 imgTuned2 = imrotate(double(cell2mat(imgsTune(4))),-90);
 imgTuned3 = imrotate(double(cell2mat(imgsTune(7))),-90);
 
+% Tuning contours
+
+imgTune2 = imrotate(double(cell2mat(imgsTune(2))),-90);
+imgTune3 = imrotate(double(cell2mat(imgsTune(3))),-90);
+imgTune5 = imrotate(double(cell2mat(imgsTune(5))),-90);
+imgTune6 = imrotate(double(cell2mat(imgsTune(6))),-90);
+imgTune8 = imrotate(double(cell2mat(imgsTune(8))),-90);
+imgTune9 = imrotate(double(cell2mat(imgsTune(9))),-90);
+
 % calculate the MAS for Tune1, Tune2 and Tune3
 [m1tune1, m2tune1, ~] = MASfunc(lmsd_map(1,:,:,:), storemask1(1,:,:,:), storemask2(1,:,:,:),5);
 [m1tune2, m2tune2, ~] = MASfunc(lmsd_map(2,:,:,:), storemask1(2,:,:,:), storemask2(2,:,:,:),5);
 [m1tune3, m2tune3, ~] = MASfunc(lmsd_map(3,:,:,:), storemask1(3,:,:,:), storemask2(3,:,:,:),5);
 
-% Figures for 
-figure;
+% Figures for Tuning
+
+
+figure(2);
+dispImage(imgTuned1);
+hold on
+imcontour(imgTune2','g') ; imcontour(imgTune3','r');
+imcontour(m1tune1','m') ; imcontour(m2tune1','c');
+ax = gca;
+exportgraphics(ax,fullfile('./final_imgs', 'MAStune1.jpg'),'Resolution',300)
+
+
+figure(3);
 dispImage(imgTuned2);
 hold on
-imcontour
-imcontour((m1tune1'), 'm');
-imcontour((m2tune1'), 'c');
+imcontour(imgTune5','g') ; imcontour(imgTune6','r');
+imcontour(m1tune2','m') ; imcontour(m2tune2','c');
+exportgraphics(ax,fullfile('./final_imgs', 'MAStune2.jpg'),'Resolution',300)
+
+figure(4);
+dispImage(imgTuned3);
+hold on
+imcontour(imgTune8','g') ; imcontour(imgTune9','r');
+imcontour(m1tune3','m') ; imcontour(m2tune3','c');
+exportgraphics(ax,fullfile('./final_imgs', 'MAStune3.jpg'),'Resolution',300)
+
+%% Calculating and displaying MAS for testing images
+
+
+sourceNumber = 5;
+
+% Optimal Parameters
+sigma_elastic = 1.4;
+sigma_fluid= 1.2;
+num_lev = 5;
+use_composition= false;
+
+% Calculating for the tuning
+[lmsd_map, storemask1, storemask2] = performRegimg(imgsAtlas, imgsTest, sigma_elastic, sigma_fluid, num_lev, use_composition, sourceNumber);
+
+% Calculating for the testing
+
+% Testing Images
+imgTest1 = imrotate(double(cell2mat(imgsTest(1))),-90);
+imgTest4 = imrotate(double(cell2mat(imgsTest(4))),-90);
+imgTest7 = imrotate(double(cell2mat(imgsTest(7))),-90);
+imgTest10 = imrotate(double(cell2mat(imgsTest(10))),-90);
+imgTest13 = imrotate(double(cell2mat(imgsTest(13))),-90);
+
+% Testing contours
+imgTest2 = imrotate(double(cell2mat(imgsTest(2))),-90);
+imgTest3 = imrotate(double(cell2mat(imgsTest(3))),-90);
+imgTest5 = imrotate(double(cell2mat(imgsTest(5))),-90);
+imgTest6 = imrotate(double(cell2mat(imgsTest(6))),-90);
+imgTest8 = imrotate(double(cell2mat(imgsTest(8))),-90);
+imgTest9 = imrotate(double(cell2mat(imgsTest(9))),-90);
+imgTest11 = imrotate(double(cell2mat(imgsTest(11))),-90);
+imgTest12 = imrotate(double(cell2mat(imgsTest(12))),-90);
+imgTest14 = imrotate(double(cell2mat(imgsTest(14))),-90);
+imgTest15 = imrotate(double(cell2mat(imgsTest(15))),-90);
+
+% calculate the MAS for testing images
+[m1test1, m2test1, ~] = MASfunc(lmsd_map(1,:,:,:), storemask1(1,:,:,:), storemask2(1,:,:,:),5);
+[m1test2, m2test2, ~] = MASfunc(lmsd_map(2,:,:,:), storemask1(2,:,:,:), storemask2(2,:,:,:),5);
+[m1test3, m2test3, ~] = MASfunc(lmsd_map(3,:,:,:), storemask1(3,:,:,:), storemask2(3,:,:,:),5);
+[m1test4, m2test4, ~] = MASfunc(lmsd_map(4,:,:,:), storemask1(4,:,:,:), storemask2(4,:,:,:),5);
+[m1test5, m2test5, ~] = MASfunc(lmsd_map(5,:,:,:), storemask1(5,:,:,:), storemask2(5,:,:,:),5);
+
+
+% Figures for Testing
+figure(1);
+dispImage(imgTest1);
+hold on
+imcontour(imgTest2','g') ; imcontour(imgTest3','r');
+imcontour(m1test1','m') ; imcontour(m2test1','c');
+ax = gca;
+exportgraphics(ax,fullfile('./final_imgs', 'MAStune1.jpg'),'Resolution',300)
+
+figure(2);
+dispImage(imgTest4);
+hold on
+imcontour(imgTest5','g') ; imcontour(imgTest6','r');
+imcontour(m1test2','m') ; imcontour(m2test2','c');
+exportgraphics(ax,fullfile('./final_imgs', 'MAStune2.jpg'),'Resolution',300)
+
+figure(3);
+dispImage(imgTest7);
+hold on
+imcontour(imgTest8','g') ; imcontour(imgTest9','r');
+imcontour(m1test3','m') ; imcontour(m2test3','c');
+exportgraphics(ax,fullfile('./final_imgs', 'MAStune3.jpg'),'Resolution',300)   
+
+figure(4);
+dispImage(imgTest10);
+hold on
+imcontour(imgTest11','g') ; imcontour(imgTest12','r');
+imcontour(m1test4','m') ; imcontour(m2test4','c');
+exportgraphics(ax,fullfile('./final_imgs', 'MAStune4.jpg'),'Resolution',300)   
+
+figure(5);
+dispImage(imgTest13);
+hold on
+imcontour(imgTest14','g') ; imcontour(imgTest15','r');
+imcontour(m1test5','m') ; imcontour(m2test5','c');
+exportgraphics(ax,fullfile('./final_imgs', 'MAStune5.jpg'),'Resolution',300)   
 
 
